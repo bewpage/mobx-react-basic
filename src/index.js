42529a4383c97f7c30cc80326eacafb88096ae8d
bewpage <b.jedlinski@bewpage.com>
2020-04-16 02:18:31 +0200

    added text

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
 
-
+// this is text added in version 1.1.0
 const employeesList = [
     {name: "John Doe", salary: 150},
     // new branch test
