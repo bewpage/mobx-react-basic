db6de51aa7f2d0b7908d153a8f68f899e54c1713
bewpage <b.jedlinski@bewpage.com>
2020-04-18 01:12:54 +0200

    add new file Footer

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,14 @@
 import React from 'react';
 
+import Header from "./Header";
+import Footer from "./Footer";
 
 const App = () => {
-    return (
-        <div>App</div>
+    return (<>
+            <Header/>
+            <div>App</div>
+            <Footer/>
+        </>
     )
 };
 
--- src/index.js
+++ src/index.js
@@ -2,18 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
 
-// here is bug #922
 const employeesList = [
     {name: "John Doe", salary: 150},
-    // new branch test
 ];
-
-
-// added text in 911 branch only for test 02
-
-
-// ad this is text in bug 911
-// new staff in release/1.1.0 branch only
 
 ReactDOM.render(
     <App/>,
