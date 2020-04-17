47780a984632c1ab8f4c49410058dfa17571bac5
bewpage <b.jedlinski@bewpage.com>
2020-04-17 15:27:17 +0200

    new commit to test squish

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
 
-
+// here is bug #922
 const employeesList = [
     {name: "John Doe", salary: 150},
     // new branch test
