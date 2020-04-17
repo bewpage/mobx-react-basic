9dd5cc144e790f4cba3983a195f3997d5a9dd146
bewpage <b.jedlinski@bewpage.com>
2020-04-17 15:27:17 +0200

    new commit to test squish

--- src/index.js
+++ src/index.js
@@ -2,13 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
 
-// this is text added in version 1.1.0
+
 const employeesList = [
     {name: "John Doe", salary: 150},
     // new branch test
 ];
 
-// new staff in release/1.1.0 branch only
 ReactDOM.render(
     <App/>,
     document.querySelector('#root')
